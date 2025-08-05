03ebfeaab2d953b835d7d60f4ad2630f5ff3670d
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-08-06 00:06:11 +0530

    Initial Commit

---
+++ FLAM Music Player app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FLAM Music Player app
+//
+//  Created by A Avinash Chidambaram on 06/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FLAM Music Player app/FLAM_Music_Player_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLAM_Music_Player_appApp.swift
+//  FLAM Music Player app
+//
+//  Created by A Avinash Chidambaram on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FLAM_Music_Player_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
